375ddde3acb70eccac43f199d4cfa442de70ed7a
Larissa Ishibashi <xa.ishibashi@gmail.com>
2025-07-08 09:48:43 -0400

    01

--- app/src/main/java/com/example/aula01lari1711/LoginActivity.kt
+++ app/src/main/java/com/example/aula01lari1711/LoginActivity.kt
@@ -12,6 +12,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var editNome: EditText
     private lateinit var editEmail: EditText
     private lateinit var editCargo: EditText
+    private lateinit var editSenha: EditText // Adicionando a senha
     private lateinit var buttonLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,22 +22,26 @@ class LoginActivity : AppCompatActivity() {
         editNome = findViewById(R.id.edit_nome)
         editEmail = findViewById(R.id.edit_email)
         editCargo = findViewById(R.id.edit_cargo)
+        editSenha = findViewById(R.id.edit_senha) // Pegando a senha
         buttonLogin = findViewById(R.id.button_login)
 
         buttonLogin.setOnClickListener {
             val nome = editNome.text.toString()
             val email = editEmail.text.toString()
             val cargo = editCargo.text.toString()
+            val senha = editSenha.text.toString() // Pegando o valor da senha
 
+            // Armazenando as informações no SharedPreferences
             val prefs = getSharedPreferences("user", Context.MODE_PRIVATE)
             with(prefs.edit()) {
                 putString("nome", nome)
                 putString("email", email)
                 putString("cargo", cargo)
+                putString("senha", senha) // Salvando a senha
                 apply()
             }
 
-            finish() // volta para MainActivity
+            finish() // Volta para a MainActivity após login
         }
     }
 }
