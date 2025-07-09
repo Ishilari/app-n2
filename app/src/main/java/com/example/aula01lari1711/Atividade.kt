63605707e4a3d81439c562505dc0556f462db6be
Larissa Ishibashi <xa.ishibashi@gmail.com>
2025-07-09 07:52:41 -0400

    02

--- app/src/main/java/com/example/aula01lari1711/Atividade.kt
+++ app/src/main/java/com/example/aula01lari1711/Atividade.kt
@@ -1,5 +1,6 @@
-package com.example.aula01lari1711
+package com.example.aula01lari1711.models
 
+//Define o modelo de dados(título, descrição, categoria, status, carga horária).
 data class Atividade(
     val titulo: String,
     val descricao: String,
