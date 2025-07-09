0d266c5dcd5a079abb3aad3fbfb78f2808dd018b
Larissa Ishibashi <xa.ishibashi@gmail.com>
2025-07-09 08:02:01 -0400

    02

--- app/src/main/java/com/example/aula01lari1711/Categoria.kt
+++ app/src/main/java/com/example/aula01lari1711/Categoria.kt
@@ -1,6 +1,9 @@
-package com.example.aula01lari1711
+package com.example.aula01lari1711.models
 
 data class Categoria(
     val nome: String,
     val minutosTotais: Int
 )
+
+
+//Representa a soma de atividades agrupadas por categoria, usada em buscas.
