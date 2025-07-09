72348fcfe23d7ce4e62f2ec9b4592bcd81aa9b76
Larissa Ishibashi <xa.ishibashi@gmail.com>
2025-07-09 07:54:02 -0400

    02

--- app/src/main/java/com/example/aula01lari1711/AtividadeRepository.kt
+++ app/src/main/java/com/example/aula01lari1711/AtividadeRepository.kt
@@ -1,7 +1,11 @@
-package com.example.aula01lari1711
+package com.example.aula01lari1711.repository
 
 import com.example.aula01lari1711.Atividade
 
 object AtividadeRepository {
     val listaAtividades = mutableListOf<Atividade>()
 }
+
+/*
+    Armazena a lista global de atividades em memória para acesso e modificação.
+ */
