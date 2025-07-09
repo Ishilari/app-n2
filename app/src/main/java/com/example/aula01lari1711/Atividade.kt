7ba4a62c5cb05bfc1384497888a8024c9153b16b
Larissa Ishibashi <xa.ishibashi@gmail.com>
2025-07-09 07:53:09 -0400

    02

--- app/src/main/java/com/example/aula01lari1711/Atividade.kt
+++ app/src/main/java/com/example/aula01lari1711/Atividade.kt
@@ -1,15 +1,9 @@
 package com.example.aula01lari1711.models
 
-//Define o modelo de dados(título, descrição, categoria, status, carga horária).
-data class Atividade(
-    val titulo: String,
-    val descricao: String,
-    val categoria: String,
-    val status: Status,
-    val cargaHorariaMinutos: Int // novo campo
+data class Categoria(
+    val nome: String,
+    val minutosTotais: Int
 )
 
-enum class Status {
-    PENDENTE,
-    VALIDADO
-}
+
+//Representa a soma de atividades agrupadas por categoria, usada em buscas.
